d98fc537bd4ea0ad7fa322fb20b9be6240b2d5c5
Fernando Romero <fernandojhoel@gmail.com>
2025-03-18 09:25:52 +0100

    checkpoint test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,32 +1,39 @@
-import { MongoClient } from "mongodb";
-import { createClient } from "./index";
+import { Client, createClient } from "./index";
 
-// jest.mock("mongodb");
+const createIndex = jest.fn();
 
 jest.mock("mongodb", () => ({
   MongoClient: jest.fn().mockImplementation(() => ({
-    db,
+    db: jest.fn().mockImplementation(() => ({
+      collection: jest.fn().mockImplementation(() => ({
+        createIndex,
+      })),
+    })),
   })),
 }));
 
-const db = jest.fn().mockImplementation(() => ({
-  collection,
-}));
-
-const collection = jest.fn().mockImplementation(() => ({
-  createIndex: jest.fn(),
-}));
-
-it("temp", async () => {
+describe("createClient", () => {
   const now = () => new Date();
   const mongoUrl = "mongodb://localhost:27017/?directConnection=true";
   const dbName = "lidex";
   const functions = new Map();
   const config = { mongoUrl, dbName };
-  const client = await createClient(functions, now, config);
+  let client: Client;
 
-  expect(db).toHaveBeenCalledTimes(1);
-  expect(db).toHaveBeenCalledWith(dbName);
-  expect(collection).toHaveBeenCalledTimes(1);
-  expect(collection).toHaveBeenCalledWith("workflows");
+  beforeEach(async () => {
+    createIndex.mockReset();
+    client = await createClient(functions, now, config);
+  });
+
+  it("creates indexes for the collection", async () => {
+    expect(createIndex).toHaveBeenNthCalledWith(1, { id: 1 }, { unique: true });
+    expect(createIndex).toHaveBeenNthCalledWith(2, { status: 1 });
+    expect(createIndex).toHaveBeenNthCalledWith(3, { status: 1, timeoutAt: 1 });
+  });
+
+  it("creates a client", async () => {
+    expect(client.start).toBeDefined();
+    expect(client.wait).toBeDefined();
+    expect(client.poll).toBeDefined();
+  });
 });
