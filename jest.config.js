9825443219c6194df15e72e3f0d5c01816e83055
Fernando Romero <fernandojhoel@gmail.com>
2025-03-21 18:35:15 +0100

    enforce 100% coverage

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
   preset: "ts-jest",
   collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/**/*.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
