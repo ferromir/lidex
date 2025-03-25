7bb28f240fc83b6184246c8ac913e7af03e6dcc1
Fernando Romero <fernandojhoel@gmail.com>
2025-03-25 17:25:36 +0100

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import { Collection, MongoClient } from "mongodb";
-import goSleep from "sleep-promise";
-
-const COLL_NAME = "workflows";
 const DEFAULT_MAX_FAILURES = 3;
 const DEFAULT_TIMEOUT_MS = 300_000; // 5m
 const DEFAULT_POLL_MS = 5_000; // 5s
-const ERROR_NAME = "LidexError";
-const MONGO_ERROR_NAME = "MongoServerError";
-const MONGO_ERROR_CODE = 11000;
-const IDLE = "idle";
-const RUNNING = "running";
-const FAILED = "failed";
-const FINISHED = "finished";
-const ABORTED = "aborted";
 
 export type Status = "idle" | "running" | "failed" | "finished" | "aborted";
 
@@ -69,157 +57,101 @@ export interface Client {
   ): Promise<Status | undefined>;
 
   /**
-   * It polls continuously the database for workflows to run.
+   * It starts polling workflows.
+   * @param shouldStop Circuit breaker for the polling loop.
    */
   poll(shouldStop: () => boolean): Promise<void>;
 }
 
-/**
- * For internal usage only.
- */
-export interface Workflow {
-  id: string;
-  handler: string;
-  input: unknown;
-  status: Status;
-  createdAt: Date;
-  timeoutAt?: Date;
-  steps?: { [key: string]: unknown };
-  naps?: { [key: string]: Date };
-  failures?: number;
-  lastError?: string;
-}
-
 export interface Config {
   handlers: Map<string, Handler>;
-  now: () => Date;
-  mongoUrl: string;
+  persistence: Persistence;
   maxFailures?: number;
   timeoutIntervalMs?: number;
   pollIntervalMs?: number;
 }
 
-class LidexError extends Error {
-  name: string;
-
-  constructor(message: string) {
-    super(message);
-    this.name = ERROR_NAME;
-  }
+interface RunData {
+  handler: string;
+  input: unknown;
+  failures?: number;
 }
 
-function makeClaim(
-  workflows: Collection<Workflow>,
-  now: () => Date,
-  timeoutIntervalMs: number
-) {
+export interface Persistence {
+  insert(workflowId: string, handler: string, input: unknown): Promise<boolean>;
+  claim(timeoutAt: Date): Promise<string | undefined>;
+  findOutput(workflowId: string, stepId: string): Promise<unknown>;
+  findWakeUpAt(workflowId: string, napId: string): Promise<Date | undefined>;
+  findRunData(workflowId: string): Promise<RunData | undefined>;
+  setAsFinished(workflowId: string): Promise<void>;
+  findStatus(workflowId: string): Promise<Status | undefined>;
+
+  updateStatus(
+    workflowId: string,
+    status: Status,
+    timeoutAt: Date,
+    failures: number,
+    lastError: string
+  ): Promise<void>;
+
+  updateOutput(
+    workflowId: string,
+    stepId: string,
+    output: unknown,
+    timeoutAt: Date
+  ): Promise<void>;
+
+  updateWakeUpAt(
+    workflowId: string,
+    napId: string,
+    wakeUpAt: Date,
+    timeoutAt: Date
+  ): Promise<void>;
+}
+
+function goSleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function makeClaim(persistence: Persistence, timeoutIntervalMs: number) {
   return async function (): Promise<string | undefined> {
-    const t = now();
-    const timeoutAt = new Date(t.getTime() + timeoutIntervalMs);
-
-    const workflow = await workflows.findOneAndUpdate(
-      {
-        $or: [
-          { status: IDLE },
-          {
-            status: { $in: [RUNNING, FAILED] },
-            timeoutAt: { $lt: t },
-          },
-        ],
-      },
-      {
-        $set: {
-          status: RUNNING,
-          timeoutAt,
-        },
-      },
-      {
-        projection: {
-          _id: 0,
-          id: 1,
-        },
-      }
-    );
-
-    return workflow?.id;
+    const now = new Date();
+    const timeoutAt = new Date(now.getTime() + timeoutIntervalMs);
+    return await persistence.claim(timeoutAt);
   };
 }
 
-function makeMakeStep(
-  workflows: Collection<Workflow>,
-  timeoutIntervalMs: number,
-  now: () => Date
-) {
+function makeMakeStep(persistence: Persistence, timeoutIntervalMs: number) {
   return function (workflowId: string) {
-    return async function <T>(id: string, fn: () => Promise<T>): Promise<T> {
-      const workflow = await workflows.findOne(
-        {
-          id: workflowId,
-        },
-        {
-          projection: {
-            _id: 0,
-            [`actions.${id}`]: 1,
-          },
-        }
-      );
+    return async function <T>(
+      stepId: string,
+      fn: () => Promise<T>
+    ): Promise<T> {
+      let output = await persistence.findOutput(workflowId, stepId);
 
-      if (!workflow) {
-        throw new LidexError(`workflow not found: ${workflowId}`);
+      if (output != undefined) {
+        return output as T;
       }
 
-      if (workflow.steps && workflow.steps[id] != undefined) {
-        return workflow.steps[id] as T;
-      }
-
-      const output = await fn();
-      const timeoutAt = new Date(now().getTime() + timeoutIntervalMs);
-
-      await workflows.updateOne(
-        {
-          id: workflowId,
-        },
-        {
-          $set: {
-            [`steps.${id}`]: output,
-            timeoutAt,
-          },
-        }
-      );
-
+      output = await fn();
+      const now = new Date();
+      const timeoutAt = new Date(now.getTime() + timeoutIntervalMs);
+      await persistence.updateOutput(workflowId, stepId, output, timeoutAt);
       return output as T;
     };
   };
 }
 
-function makeMakeSleep(
-  workflows: Collection<Workflow>,
-  timeoutIntervalMs: number,
-  goSleep: (ms: number) => Promise<void>,
-  now: () => Date
-) {
+function makeMakeSleep(persistence: Persistence, timeoutIntervalMs: number) {
   return function (workflowId: string) {
-    return async function (id: string, ms: number): Promise<void> {
-      const workflow = await workflows.findOne(
-        {
-          id: workflowId,
-        },
-        {
-          projection: {
-            _id: 0,
-            [`naps.${id}`]: 1,
-          },
-        }
-      );
+    return async function (napId: string, ms: number): Promise<void> {
+      let wakeUpAt = await persistence.findWakeUpAt(workflowId, napId);
+      const now = new Date();
 
-      if (!workflow) {
-        throw new LidexError(`workflow not found: ${workflowId}`);
-      }
-
-      const t = now();
-
-      if (workflow.naps && workflow.naps[id]) {
-        const remainingMs = workflow.naps[id].getTime() - t.getTime();
+      if (wakeUpAt) {
+        const remainingMs = wakeUpAt.getTime() - now.getTime();
 
         if (remainingMs > 0) {
           await goSleep(remainingMs);
@@ -228,63 +160,38 @@ function makeMakeSleep(
         return;
       }
 
-      const sleepUntil = new Date(t.getTime() + ms);
-      const timeoutAt = new Date(sleepUntil.getTime() + timeoutIntervalMs);
-
-      await workflows.updateOne(
-        {
-          id: workflowId,
-        },
-        {
-          $set: {
-            [`naps.${id}`]: sleepUntil,
-            timeoutAt,
-          },
-        }
-      );
-
+      wakeUpAt = new Date(now.getTime() + ms);
+      const timeoutAt = new Date(wakeUpAt.getTime() + timeoutIntervalMs);
+      await persistence.updateWakeUpAt(workflowId, napId, wakeUpAt, timeoutAt);
       await goSleep(ms);
     };
   };
 }
 
 function makeRun(
-  workflows: Collection<Workflow>,
+  persistence: Persistence,
   handlers: Map<string, Handler>,
   makeStep: (
     workflowId: string
-  ) => <T>(actionId: string, fn: () => Promise<T>) => Promise<T>,
+  ) => <T>(stepId: string, fn: () => Promise<T>) => Promise<T>,
   makeSleep: (
     workflowId: string
   ) => (napId: string, ms: number) => Promise<void>,
-  now: () => Date,
   start: <T>(id: string, handler: string, input: T) => Promise<boolean>,
   maxFailures: number,
   timeoutIntervalMs: number
 ) {
   return async function (workflowId: string): Promise<void> {
-    const workflow = await workflows.findOne(
-      {
-        id: workflowId,
-      },
-      {
-        projection: {
-          _id: 0,
-          handler: 1,
-          input: 1,
-          failures: 1,
-        },
-      }
-    );
+    const runData = await persistence.findRunData(workflowId);
 
-    if (!workflow) {
-      throw new LidexError(`workflow not found: ${workflowId}`);
+    if (!runData) {
+      throw new Error(`workflow not found: ${workflowId}`);
     }
 
-    const fn = handlers.get(workflow.handler);
+    const fn = handlers.get(runData.handler);
 
     if (!fn) {
-      throw new LidexError(`handler not found: ${workflow.handler}`);
+      throw new Error(`handler not found: ${runData.handler}`);
     }
 
     const ctx: Context = {
@@ -294,16 +201,8 @@ function makeRun(
     };
 
     try {
-      await fn(ctx, workflow.input);
-
-      await workflows.updateOne(
-        {
-          id: workflowId,
-        },
-        {
-          $set: { status: FINISHED },
-        }
-      );
+      await fn(ctx, runData.input);
+      await persistence.setAsFinished(workflowId);
     } catch (error) {
       let lastError = "";
 
@@ -313,83 +212,44 @@ function makeRun(
         lastError = JSON.stringify(error);
       }
 
-      const failures = (workflow.failures || 0) + 1;
-      const status = failures < maxFailures ? FAILED : ABORTED;
-      const t = now();
-      const timeoutAt = new Date(t.getTime() + timeoutIntervalMs);
+      const failures = (runData.failures || 0) + 1;
+      const status = failures < maxFailures ? "failed" : "aborted";
+      const now = new Date();
+      const timeoutAt = new Date(now.getTime() + timeoutIntervalMs);
 
-      await workflows.updateOne(
-        {
-          id: workflowId,
-        },
-        {
-          $set: {
-            status,
-            timeoutAt,
-            failures,
-            lastError,
-          },
-        }
+      await persistence.updateStatus(
+        workflowId,
+        status,
+        timeoutAt,
+        failures,
+        lastError
       );
     }
   };
 }
 
-function makeStart(workflows: Collection<Workflow>, now: () => Date) {
+function makeStart(persistence: Persistence) {
   return async function <T>(
-    id: string,
+    workflowId: string,
     handler: string,
     input: T
   ): Promise<boolean> {
-    try {
-      await workflows.insertOne({
-        id,
-        handler,
-        input,
-        status: IDLE,
-        createdAt: now(),
-      });
-
-      return true;
-    } catch (error) {
-      const e = error as { name: string; code: number };
-
-      // Workflow already started, ignore.
-      if (e.name === MONGO_ERROR_NAME && e.code === MONGO_ERROR_CODE) {
-        return false;
-      }
-
-      throw error;
-    }
+    return persistence.insert(workflowId, handler, input);
   };
 }
 
-function makeWait(
-  workflows: Collection<Workflow>,
-  goSleep: (ms: number) => Promise<void>
-) {
+function makeWait(persistence: Persistence) {
   return async function (
-    id: string,
+    workflowId: string,
     status: Status[],
     times: number,
     ms: number
   ): Promise<Status | undefined> {
     for (let i = 0; i < times; i++) {
-      const workflow = await workflows.findOne(
-        {
-          id,
-          status: { $in: status },
-        },
-        {
-          projection: {
-            _id: 0,
-            status: 1,
-          },
-        }
-      );
+      const found = await persistence.findStatus(workflowId);
 
-      if (workflow) {
-        return workflow.status;
+      if (found && status.includes(found)) {
+        return found;
       }
 
       await goSleep(ms);
@@ -402,7 +262,6 @@ function makeWait(
 function makePoll(
   claim: () => Promise<string | undefined>,
   run: (workflowId: string) => Promise<void>,
-  goSleep: (ms: number) => Promise<void>,
   pollIntervalMs: number
 ) {
   return async function (shouldStop: () => boolean): Promise<void> {
@@ -425,34 +284,27 @@ function makePoll(
  * @returns The client instance.
  */
 export async function makeClient(config: Config): Promise<Client> {
-  const { now, handlers, mongoUrl } = config;
+  const { handlers, persistence } = config;
   const maxFailures = config.maxFailures || DEFAULT_MAX_FAILURES;
   const timeoutIntervalMs = config.timeoutIntervalMs || DEFAULT_TIMEOUT_MS;
   const pollIntervalMs = config.pollIntervalMs || DEFAULT_POLL_MS;
-  const mongo = new MongoClient(mongoUrl);
-  const db = mongo.db();
-  const workflows = db.collection<Workflow>(COLL_NAME);
-  await workflows.createIndex({ id: 1 }, { unique: true });
-  await workflows.createIndex({ status: 1 });
-  await workflows.createIndex({ status: 1, timeoutAt: 1 });
-  const start = makeStart(workflows, now);
-  const wait = makeWait(workflows, goSleep);
-  const claim = makeClaim(workflows, now, timeoutIntervalMs);
-  const makeStep = makeMakeStep(workflows, timeoutIntervalMs, now);
-  const makeSleep = makeMakeSleep(workflows, timeoutIntervalMs, goSleep, now);
+  const start = makeStart(persistence);
+  const wait = makeWait(persistence);
+  const claim = makeClaim(persistence, timeoutIntervalMs);
+  const makeStep = makeMakeStep(persistence, timeoutIntervalMs);
+  const makeSleep = makeMakeSleep(persistence, timeoutIntervalMs);
 
   const run = makeRun(
-    workflows,
+    persistence,
     handlers,
     makeStep,
     makeSleep,
-    now,
     start,
     maxFailures,
     timeoutIntervalMs
   );
 
-  const poll = makePoll(claim, run, goSleep, pollIntervalMs);
+  const poll = makePoll(claim, run, pollIntervalMs);
   return { start, wait, poll };
 }
 
