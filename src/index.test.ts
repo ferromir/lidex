5a20cd2426d523a791ee103f40bbe2a4ce3148a5
Fernando Romero <fernandojhoel@gmail.com>
2025-03-29 12:59:18 +0100

    refactor test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,22 +1,23 @@
 import { forInternalTesting, Persistence } from "./index";
 import { mock } from "jest-mock-extended";
 
-const { makeClaim } = forInternalTesting;
-
 describe("claim", () => {
+  const { makeClaim } = forInternalTesting;
+  const persistence = mock<Persistence>();
+  const timeoutIntervalMs = 10;
+  const claim = makeClaim(persistence, timeoutIntervalMs);
+
+  beforeEach(() => {
+    persistence.claim.mockReset();
+  });
+
   it("returns the workflow id if found", async () => {
-    const persistence = mock<Persistence>();
     persistence.claim.mockResolvedValue("workflow-1");
-    const timeoutIntervalMs = 10;
-    const claim = makeClaim(persistence, timeoutIntervalMs);
     const workflowId = await claim();
     expect(workflowId).toEqual("workflow-1");
   });
 
   it("returns undefined if not found", async () => {
-    const persistence = mock<Persistence>();
-    const timeoutIntervalMs = 10;
-    const claim = makeClaim(persistence, timeoutIntervalMs);
     const workflowId = await claim();
     expect(workflowId).toBeUndefined();
   });
