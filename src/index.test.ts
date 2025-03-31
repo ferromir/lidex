a708cb8b55cd69ef3800b4e588b352c4187047ba
Fernando Romero <fernandojhoel@gmail.com>
2025-03-31 16:20:25 +0200

    test cleanup

--- src/index.test.ts
+++ src/index.test.ts
@@ -40,11 +40,8 @@ describe("claim", () => {
     const claim = makeClaim(persistence, timeoutIntervalMs);
     const workflowId = await claim();
     expect(workflowId).toEqual("workflow-1");
-    const _now = persistence.claim.mock.calls[0][0];
-    expect(_now).toEqual(now);
-    const _timeoutAt = persistence.claim.mock.calls[0][1];
     const timeout = new Date(now.getTime() + timeoutIntervalMs);
-    expect(_timeoutAt).toEqual(timeout);
+    expect(persistence.claim).toHaveBeenCalledWith(now, timeout);
   });
 });
 
@@ -56,7 +53,7 @@ describe("step", () => {
     const fn = jest.fn();
     const output = await step("step-1", fn);
     expect(output).toEqual("output-1");
-    expect(fn).not.toHaveBeenCalled();
+    expect(persistence.findOutput).toHaveBeenCalledWith("workflow-1", "step-1");
   });
 
   it("executes function if output not found", async () => {
@@ -65,7 +62,16 @@ describe("step", () => {
     const fn = jest.fn().mockResolvedValue("output-1");
     const output = await step("step-1", fn);
     expect(output).toEqual("output-1");
-    expect(fn).toHaveBeenCalledTimes(1);
+    expect(persistence.findOutput).toHaveBeenCalledWith("workflow-1", "step-1");
+    expect(fn).toHaveBeenCalled();
+    const timeoutAt = new Date(now.getTime() + timeoutIntervalMs);
+
+    expect(persistence.updateOutput).toHaveBeenCalledWith(
+      "workflow-1",
+      "step-1",
+      "output-1",
+      timeoutAt,
+    );
   });
 });
 
@@ -75,7 +81,11 @@ describe("sleep", () => {
     const makeSleep = makeMakeSleep(persistence, timeoutIntervalMs);
     const sleep = makeSleep("workflow-1");
     await sleep("nap-1", 1000);
-    expect(setTimeout).not.toHaveBeenCalled();
+
+    expect(persistence.findWakeUpAt).toHaveBeenCalledWith(
+      "workflow-1",
+      "nap-1",
+    );
   });
 
   it("sleeps when the nap is found but not completed", async () => {
@@ -84,6 +94,12 @@ describe("sleep", () => {
     const makeSleep = makeMakeSleep(persistence, timeoutIntervalMs);
     const sleep = makeSleep("workflow-1");
     await sleep("nap-1", 1000);
+
+    expect(persistence.findWakeUpAt).toHaveBeenCalledWith(
+      "workflow-1",
+      "nap-1",
+    );
+
     expect(setTimeout).toHaveBeenCalledWith(expect.anything(), 2000);
   });
 
@@ -93,6 +109,11 @@ describe("sleep", () => {
     await sleep("nap-1", 1000);
     const wakeUpAt = new Date(now.getTime() + 1000);
     const timeoutAt = new Date(wakeUpAt.getTime() + timeoutIntervalMs);
+
+    expect(persistence.findWakeUpAt).toHaveBeenCalledWith(
+      "workflow-1",
+      "nap-1",
+    );
 
     expect(persistence.updateWakeUpAt).toHaveBeenCalledWith(
       "workflow-1",
